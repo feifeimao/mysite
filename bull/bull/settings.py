8af134f7464ad35d10ef18c3ea77a3eeb8acf0c8
feifei <feifeimao0610@126.com>
2016-03-28 10:29:42 +0800

    begin using rest_framework,take snippets for example

--- bull/bull/settings.py
+++ bull/bull/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
     'rest_framework',
+    'snippets.apps.SnippetsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
